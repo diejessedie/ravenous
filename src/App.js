0e2ac426467a9e5fd78053679482db11be6aee5e
Jess <jjohnson4927@gmail.com>
2017-11-16 12:26:43 -0600

    add searchbar and businesslist to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello Codecademy!
-        </p>
-      </div>
+  <h1>ravenous</h1>
+  <SearchBar />
+  <BusinessList />
+</div>
     );
   }
 }
